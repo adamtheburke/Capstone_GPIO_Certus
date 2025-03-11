2ca1d83ddb065dbdc380437e587e22808bb1f702
RR-4L <rromero56809@gmail.com>
2025-03-11 14:03:40 -0700

    Update PowerSupply.py

--- PowerSupply.py
+++ PowerSupply.py
@@ -4,7 +4,9 @@ import pyvisa
 rm = pyvisa.ResourceManager()
 P_sup = rm.open_source('name of o POWERSUPPLY string')
 
-
+#Function will be used to set the Voltage and amps of a specific channel
+#CH1 max voltage = 6.18 V, Min = 0 V, Max Amp = 5.15 A, Min Amp = 0.002 A
+#CH2/3 max voltage = 30.9 V, Min = 0 V, Max Amp = 1.03 A, Min Amp = 0.001 A
 def Ch_Set_Vol_Amp (Voltage, Amp, channel):
         
         P_sup = rm.open_source('name of o POWERSUPPLY string')
@@ -18,5 +20,11 @@ def Ch_Set_Vol_Amp (Voltage, Amp, channel):
         V = str(Voltage)
         Command = "APPL "+CH+", "+V+", "+A
         return P_sup.write(Command)
+
+def Rest_P_Sup(): 
+        
+
+
+        return
 
 
