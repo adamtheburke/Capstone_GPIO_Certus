c1579ab1bf92d997545b9dbfcd69404192bf5cfd
RR-4L <rromero56809@gmail.com>
2025-03-10 18:43:11 -0700

    Update PowerSupply.py

--- PowerSupply.py
+++ PowerSupply.py
@@ -1,2 +1,20 @@
-import pyvisa 
+import pyvisa
 
+##These will need to be edited fot the rest of the Program to work. 
+rm = pyvisa.ResourceManager()
+P_sup = rm.open_source('name of o POWERSUPPLY string')
+
+
+def powersupply (Voltage, Amp, channel):
+
+    Output = P_sup = rm.open_source('name of o POWERSUPPLY string')
+    if channel == 1:
+            CH =  'CH1'
+    elif channel == 2:
+            CH =  'CH2'
+    else:
+            CH =  'CH3'
+    Output.write("APPL CH, Voltage, Amp ")
+
+
+    return Output
